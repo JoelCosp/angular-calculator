f24ae88088721618dac20b988218d63c999b20a5
JoelVRV <joelc456789@gmail.com>
2025-03-18 11:11:11 +0100

    CALCULADORA FUNCIONAL

--- calculator/src/app/components/calculator/calculator.component.ts
+++ calculator/src/app/components/calculator/calculator.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  title = 'Calculator'; // Titulo dinamico
+  display: string = ''; // Visor de numeros
 
+  // Añadir elemento al string del visor
+  appendInput(value: string): void {
+    this.display += value;
+  }
+  // Limpiar el visor
+  clear(): void {
+    this.display = '';
+  }
+  // Calcular
+  calculate(): void {
+    try {
+      this.display = eval(this.display); // Evalua el string y hace el calculo
+    } catch (e) {
+        this.display = 'Error';
+    }
+  }
 }
